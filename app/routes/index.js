9d827ab7407a568efae323ecd998f7a301425f0a
Armen <danielyan_armen@yahoo.com>
2015-11-11 17:52:06 +0400

    update

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
+var mysql = require('mysql');
+
+/*Create mysql connection*/
+var conn = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : ''
+});
+conn.connect(function(err) {
+  if (err) {
+    console.error('error connecting: ' + err.stack);
+    return;
+  } else {
+    console.log('connected as id ' + conn.threadId);
+  }
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+
 });
 
 module.exports = router;
